fa42762efcf9e419ffc7c50c782cd4e7b47f670e
Sawerick <tominek9@centrum.sk>
2013-10-27 18:09:51 +0100

    Created new integrated _getCharacterGroup, and also created new _getOperands based on previously mentioned fnc

--- engine.js
+++ engine.js
@@ -1,5 +1,6 @@
 // ================== CONSTANTS ====================
 const OPERATORS = [ "+", "-", "*", "/" ];
+const OPERANDS = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
 
 // Our expression MUST BE without whitespace because of simplicity
 function _removeWhitespace(str)
@@ -7,19 +8,31 @@ function _removeWhitespace(str)
   return str.replace(/\s+/g, ""); 
 }
 
-function _getFoundOperatorsList(str)
+function __getCharacterGroup(ALLOWABLE_LIST, str)
 {
-   var foundOps = [];
+   var found = [];
    for ( index in str )
-     if ( OPERATORS.indexOf(str[index]) != -1 )
-       foundOps.push( str[index] );
-   return foundOps;
+     if ( ALLOWABLE_LIST.indexOf(str[index]) != -1 )
+       found.push( str[index] );
+   return found;
+}
+
+function _getOperators(str)
+{
+    return __getCharacterGroup(OPERATORS, str);
+}
+
+function _getOperands(str)
+{
+    return __getCharacterGroup(OPERANDS, str);
 }
 
 // Main evalvating function
 function evalvate(str)
 { 
-  _removeWhitespace(str);  
+  str = _removeWhitespace(str);
+  var operators = _getOperators(str);
+  var operands  = _getOperands(str);
 }
 
 
