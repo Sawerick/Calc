57f6b284855a2cce651f764c5140d58be55c8044
Sawerick <tominek9@centrum.sk>
2013-10-27 18:53:26 +0100

    Fixed tests and OPERAND_PATT regex constant

--- engine.js
+++ engine.js
@@ -2,7 +2,7 @@
 const OPERATORS     = [ "+", "-", "*", "/" ];
 const OPERANDS      = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
 const OPERATOR_PATT = /[\+\-\*\/]/g;
-const OPERAND_PATT  = /[0-9]+(?:[.][0-9]+){,1}/g;
+const OPERAND_PATT  = /[0-9]+(?:[.][0-9]+){0,1}/g;
 
 // Our expression MUST BE without whitespace because of simplicity
 function _removeWhitespace(str)
@@ -43,20 +43,18 @@ function _removeWhitespace_test()
   alert(_removeWhitespace("chnaa") === "chnaa");
 }
 
-function _getFoundOperatorsList_test()
+function _getOperators_test()
 {
-  alert(_getFoundOperatorsList("jan+o-"));   
-  alert(_getFoundOperatorsList("ja"));
-  alert(_getFoundOperatorsList("jan+o*--"));
-  alert(_getFoundOperatorsList("+++"));
+  alert(_getOperators("jan+o-"));   
+  alert(_getOperators("ja"));
+  alert(_getOperators("jan+o*--"));
+  alert(_getOperators("+++"));
 }
 
-function _getFoundOperandsList_test()
+function _getOperands_test()
 {
-  alert(_getFoundOperatorsList("75+6"));   
-  alert(_getFoundOperatorsList("hmm"));
-  alert(_getFoundOperatorsList("-6+54.8*6"));
-  alert(_getFoundOperatorsList("+2.00+0.1+35"));
+  alert(_getOperands("75+6"));   
+  alert(_getOperands("hmm"));
+  alert(_getOperands("-6+54.8*6"));
+  alert(_getOperands("+2.00+0.1+35"));
 }
-
-
