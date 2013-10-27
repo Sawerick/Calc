a675ef5c3ec6bd7af0b855898a56bca45596e6d1
Sawerick <tominek9@centrum.sk>
2013-10-27 16:40:20 +0100

    Added function for retrieving list of operators in expressions

--- engine.js
+++ engine.js
@@ -1,4 +1,25 @@
-function removeWhitespace(str)
+// ================== CONSTANTS ====================
+const OPERATORS = [ "+", "-", "*", "/" ];
+
+// Our expression MUST BE without whitespace because of simplicity
+function _removeWhitespace(str)
 {
   return str.replace(/\s+/g, ""); 
 }
+
+function _getFoundOperatorsList(str)
+{
+   var foundOps = [];
+   for ( index in str )
+     if ( OPERATORS.indexOf(str[index]) != -1 )
+       foundOps.push( str[index] );
+   return foundOps;
+}
+
+// Main evalvating function
+function evalvate(str)
+{
+  _removeWhitespace(str); 
+}
+
+
